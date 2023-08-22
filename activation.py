a9240b672ef10459c9917364558ebbfefcfd279b
Li <56581796+NWPU-Li@users.noreply.github.com>
2023-08-22 19:20:48 +0800

    Create activation.py

---
+++ activation.py
@@ -0,0 +1,29 @@
+# Copyright 2023 LL.
+
+import torch.nn as nn
+
+activations = {
+    "ReLU": nn.ReLU,
+    "LeakyReLU": nn.LeakyReLU,
+    "ReLU6": nn.ReLU6,
+    "SELU": nn.SELU,
+    "ELU": nn.ELU,
+    "GELU": nn.GELU,
+    "PReLU": nn.PReLU,
+    "SiLU": nn.SiLU,
+    "HardSwish": nn.Hardswish,
+    "Hardswish": nn.Hardswish,
+    None: nn.Identity,
+}
+
+
+def act_layers(name):
+    assert name in activations.keys()
+    if name == "LeakyReLU":
+        return nn.LeakyReLU(negative_slope=0.1, inplace=True)
+    elif name == "GELU":
+        return nn.GELU()
+    elif name == "PReLU":
+        return nn.PReLU()
+    else:
+        return activations[name](inplace=True)
